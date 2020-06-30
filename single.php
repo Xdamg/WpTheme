b39cc8cc1d503acf243d3a885eddbd152003ab5b
Rhalloub Taha <taha.rhalloub01@gmail.com>
2020-06-30 21:06:56 +0100

    Another Commit

--- single.php
+++ single.php
@@ -3,17 +3,15 @@
 <div class="w3-col l8 s12">
 
 
-        <div class="w3-card-4 w3-margin w3--light-grey">
-
             <?php if (has_post_thumbnail($post->ID)): ?>
             <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
 
             <img src="<?php echo $image[0]; ?>" alt="Nature" style="width:100%">
 
-        </div>
+        
         <?php endif; ?>
 
-
+  <div class="w3-card-4 w3-margin w3--light-grey">
         <div class="w3-container">
             <h3><b><?php the_title(); ?></b></h3>
            
@@ -25,14 +23,17 @@
            <p> <?php echo get_post_field('post_content', the_ID()); ?></p>
            
         </div>
+		</div>
     </div>
-    <hr>
-<div>
 
+	<div>
+<br><br><br><br><br>
+<div>
 <?php comments_template(); ?>
 
 
     </div>
+
     
 
 
